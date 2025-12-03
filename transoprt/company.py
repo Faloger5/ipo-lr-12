b93d4d5a851cebf6c18d72165eced09fff36b2fe
faloger <gtllop098@gmail.com>
2025-12-03 12:04:34 +0300

    Update company.py

--- transoprt/company.py
+++ transoprt/company.py
@@ -4,3 +4,9 @@ class TransportCompany:
         self.name = name # Сохраняем название компании
         self.vehicles = [] # Список транспортных средств
         self.clients = [] # Список клиентов
+     def __str__(self):
+        # Магический метод для строкового представления компании
+        return (f"Транспортная компания: {self.name}\n"
+                f"Количество транспортных средств: {len(self.vehicles)}\n"
+                f"Количество клиентов: {len(self.clients)}")
+
