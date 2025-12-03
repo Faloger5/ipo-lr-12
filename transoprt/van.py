5f8979eb9ed8b1739a19f1598c2ebf81981e6321
faloger <gtllop098@gmail.com>
2025-12-03 18:49:17 +0300

    Update van.py

--- transoprt/van.py
+++ transoprt/van.py
@@ -1,5 +1,9 @@
+from .vehicle import Vehicle  # добавлен импорт родительского класса
+
 class Van(Vehicle):
     # Класс фургона, наследует Vehicle
     def __init__(self, capacity, is_refrigerated):
-        super().__init__(capacity) # Вызываем конструктор родителя Vehicle
-        self.is_refrigerated = is_refrigerated # Сохраняем признак наличия холодильника
+        super().__init__(capacity)  # Вызываем конструктор родителя Vehicle
+        if not isinstance(is_refrigerated, bool):  # добавлена проверка типа
+            raise TypeError("is_refrigerated должен быть True или False")
+        self.is_refrigerated = is_refrigerated  # Сохраняем признак наличия холодильника
