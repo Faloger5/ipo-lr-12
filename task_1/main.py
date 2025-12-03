ec3239bb53941a2e8f9cbfb1b8aaa7504704080e
faloger <gtllop098@gmail.com>
2025-12-03 11:20:25 +0300

    Create main.py

---
+++ task_1/main.py
@@ -0,0 +1,6 @@
+class Client:
+    # Класс клиента, у которого есть имя, вес груза и статус VIP
+    def __init__(self, name, cargo_weight, is_vip=False):
+        self.name = name # Сохраняем имя клиента
+        self.cargo_weight = cargo_weight # Сохраняем вес груза клиента
+        self.is_vip = is_vip # Сохраняем статус VIP
