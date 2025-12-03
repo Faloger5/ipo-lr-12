38c52d89dbab799648574822d882821309fbf0dc
faloger <gtllop098@gmail.com>
2025-12-03 22:34:30 +0300

    Update van.py

--- transoprt/van.py
+++ transoprt/van.py
@@ -7,3 +7,6 @@ class Van(Vehicle):
         if not isinstance(is_refrigerated, bool):  # добавлена проверка типа
             raise TypeError("is_refrigerated должен быть True или False")
         self.is_refrigerated = is_refrigerated  # Сохраняем признак наличия холодильника
+        
+    def __str__(self):
+        return super().__str__() + f"\nХолодильник: {self.is_refrigerator}" # Выводим параметр холодильника
