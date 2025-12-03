a9807bcd4996ad472587b5dbd6756b54edb7849d
faloger <gtllop098@gmail.com>
2025-12-03 18:48:34 +0300

    Update client.py

--- transoprt/client.py
+++ transoprt/client.py
@@ -1,9 +1,17 @@
 class Client:
     # Класс клиента, у которого есть имя, вес груза и статус VIP
     def __init__(self, name, cargo_weight, is_vip=False):
-        self.name = name # Сохраняем имя клиента
-        self.cargo_weight = cargo_weight # Сохраняем вес груза клиента
-        self.is_vip = is_vip # Сохраняем статус VIP
+        if not isinstance(name, str):  # добавлена проверка типа имени
+            raise TypeError("Имя клиента должно быть строкой")
+        if not isinstance(cargo_weight, (int, float)):  # добавлена проверка типа веса
+            raise TypeError("Вес груза должен быть числом")
+        if cargo_weight <= 0:  # добавлена проверка на положительный вес
+            raise ValueError("Вес груза должен быть положительным")
+
+        self.name = name  # Сохраняем имя клиента
+        self.cargo_weight = cargo_weight  # Сохраняем вес груза клиента
+        self.is_vip = is_vip  # Сохраняем статус VIP
+
     def __str__(self):
         # Магический метод для строкового представления объекта
         return f"Клиент {self.name}, груз: {self.cargo_weight} тонн, VIP: {self.is_vip}"
