4e0c099e39fac52ff2d7531613e8999ad45ea108
faloger <gtllop098@gmail.com>
2025-12-03 18:49:05 +0300

    Update airplane.py

--- transoprt/airplane.py
+++ transoprt/airplane.py
@@ -1,5 +1,9 @@
+from .vehicle import Vehicle  # добавлен импорт родительского класса
+
 class Airplane(Vehicle):
     # Класс самолета, наследует Vehicle
     def __init__(self, capacity, max_altitude):
-        super().__init__(capacity) # Вызываем конструктор родителя Vehicle
-        self.max_altitude = max_altitude # Сохраняем максимальную высоту полета
+        super().__init__(capacity)  # Вызываем конструктор родителя Vehicle
+        if not isinstance(max_altitude, int):  # добавлена проверка типа высоты
+            raise TypeError("Максимальная высота должна быть целым числом")
+        self.max_altitude = max_altitude  # Сохраняем максимальную высоту полета
